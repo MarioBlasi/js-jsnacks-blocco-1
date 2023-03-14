d8161f2f506995fbf8689322955d61dcc436dc43
MarioBlasi <mblasi509@gmail.com>
2023-03-14 10:47:06 +0000

    //Snack 1 Blocco 1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,1 +1,11 @@
-alert("welcome");
+//Snack 1 Blocco 1
+// L’utente inserisce due numeri in successione, con due prompt.Il software stampa il maggiore.
+
+let num1 = prompt("Inserisci il primo numero:");
+let num2 = prompt("Inserisci il secondo numero:");
+
+if (num1 > num2) {
+  console.log(num1);
+} else {
+  console.log(num2);
+}
