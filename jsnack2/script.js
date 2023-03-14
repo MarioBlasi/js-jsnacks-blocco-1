f641382288afa4d8a5e3cdcf09a6beec1c8254ea
MarioBlasi <mblasi509@gmail.com>
2023-03-14 11:18:51 +0000

    jsnack2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,1 +1,15 @@
-alert("welcome");
+// L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+let primaParola = prompt("Inserisci la prima parola:");
+let secondaParola = prompt("Inserisci la seconda parola:");
+console.log("Le due parole inserite sono:", primaParola, secondaParola);
+
+//  usiamo length per contare la lunghezza di una parola inserita dall'utente:
+if (primaParola.length < secondaParola.length) {
+  console.log(primaParola);
+  console.log(secondaParola);
+} else {
+  console.log(secondaParola);
+  console.log(primaParola);
+}
