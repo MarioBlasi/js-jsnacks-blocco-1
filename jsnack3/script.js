d3e0bf57c78c0773128438b067204e7665fb809c
MarioBlasi <mblasi509@gmail.com>
2023-03-14 11:51:27 +0000

    SNACK 3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,1 +1,14 @@
-alert("welcome");
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+// chiediamo 10 volte all 'utente di inserire un numero
+// usiamo il ciclo for
+
+let numeri = [];
+
+for (let i = 0; i < 10; i++) {
+  let numero = prompt("Inserisci un numero:");
+  numeri.push(numero);
+}
+
+console.log("I numeri inseriti sono:", numeri);
