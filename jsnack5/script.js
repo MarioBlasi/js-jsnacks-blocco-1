0f6339269efc0a194d59d8c4ae26770062da4b9d
MarioBlasi <mblasi509@gmail.com>
2023-03-15 11:50:34 +0000

    jsnak5

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -1,1 +1,19 @@
-alert("welcome");
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array.
+
+let sommaPari = 0,
+  sommaDispari = 0;
+
+let a;
+
+for (let i = 0; i < 6; i++) {
+  a = parseInt(prompt("Inserisci un numero:"));
+  if (a % 2 == 0) {
+    sommaPari += a;
+  } else {
+    sommaDispari += a;
+  }
+}
+console.log(sommaPari);
+console.log(sommaDispari);
