758964dcfb0ff457dc8df35c064b41a027d96465
MarioBlasi <mblasi509@gmail.com>
2023-03-15 10:52:17 +0000

    eser while

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -10,5 +10,18 @@ for (let i = 0; i < 10; i++) {
   let numero = prompt("Inserisci un numero:");
   numeri.push(numero);
 }
-
 console.log("I numeri inseriti sono:", numeri);
+//SNACK 3
+
+/*Rifare con WHILE LOOP:  Il software deve chiedere per 10 
+volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+let i = 0;
+
+while (k < 10) {
+  const thisnumeri = numeri[i];
+  console.log(thisnumeri);
+  k++;
+}
+console.log("I numeri inseriti sono while :", numeri);
